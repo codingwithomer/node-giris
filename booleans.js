9b6f1122ac2f06f74122dade31ff503c7164c0e8
unknown <omerbilalcan@gmail.com>
2019-09-08 23:12:25 +0300

    booleanlar ile ilgili çalışma eklendi.

---
+++ booleans.js
@@ -0,0 +1,11 @@
+//Bir ifade ya 1'dir ya da 0'dır. True ya da false'tur.
+
+// Karşılaştırma operatörleri < >  <= => == != === !== !
+
+var status = true;
+// var status = false;
+
+// console.log(status); <== output false'tur. Çünkü ilk değer ezilmiştir.
+
+if(!status) 
+    console.log("Şart sağlandı...");
